cfb8be3ef52f029beca1fb0959ebac1dbdc6c91e
Emily F <72047699+EmilyF99@users.noreply.github.com>
2024-04-27 00:53:30 +0100

    Add files via upload

---
+++ SettingUpAllegro/source.cpp
@@ -0,0 +1,9 @@
+#include <allegro5/allegro5.h>
+#include <conio.h>
+#include <stdio.h>
+
+int main() {
+	printf("Greetings Eathlings.\n");
+	printf("All your base are belong to us!\n");
+	_getch(); //Wait for Character
+}
